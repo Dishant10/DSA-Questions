a5999bd1e7cb825104baead5baa5da4d54b8b50b
Dishant Nagpal <dishantnagpal10@gmail.com>
2022-12-23 18:48:23 +0530

    23-Dec-22,1

--- distinctAdjacent.cpp
+++ distinctAdjacent.cpp
@@ -4,53 +4,27 @@ int main(){
   int n;
   cin>>n;
   vector<int> v;
-  unordered_map<int,int> m;
-  set<int> s;
-  int count=0;
   for(int i=0;i<n;++i){
     int temp;
     cin>>temp;
-    m[temp]++;
-    s.insert(temp);
     v.push_back(temp);
   }
-  if(n==1){
-    cout<<"YES1";
-    return 0;
+  sort(v.begin(),v.end());
+  int continuous=1,maxContinuous=1;
+  for(int i=1;i<n;++i){
+    if(v[i]==v[i-1]){
+      continuous++;
+      if(continuous>maxContinuous){
+        maxContinuous=continuous;
+      }
+    }else {
+      continuous=1;
+    }
+  }
+  if(maxContinuous<=(n+1)/2){
+    cout<<"YES";
   }
   else{
-    for(int i=0;i<n;++i){
-      if(m[v[i]]>(n/2)+2){
-        cout<<"NO1";
-        return 0;
-        break;
-      }
-      else if(m[v[i]]==1){
-        count++;
-      }
-    }
-    if(count==n){
-      cout<<"YES2";
-      return 0;
-    }
-    for(auto ele:s){
-      cout<<ele<<" "<<m[ele]<<" ";
-    }
-    cout<<"\n";
-    cout<<"\n";
-    for(auto ele:s){
-      if(m[ele]>=2 ){
-        int spaces=m[ele];
-        if(count!=0){
-          count=count-(spaces-1);
-        }
-        else if(count==0){
-          cout<<"NO2";
-          return 0;
-          break;
-        }
-      }
-    }
-    cout<<"YES3";
+    cout<<"NO";
   }
 }
